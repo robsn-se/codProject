700c189056ad2435418ba175d5555e7520954f47
robsn-se <rubikserobyan17@mail.ru>
2023-02-11 15:33:14 +0300

    view page

--- index.php
+++ index.php
@@ -1,2 +1,50 @@
-<?php
-echo "jgyyytgt";
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/main.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="img">
+    <li>
+        <a href=""><img src="images/1.jpg" alt=""></a>
+        <div>
+            <h6>52242742</h6>
+            <p>sgfdhjgkhgdfg</p>
+        </div>
+    </li>
+    <li>
+        <img src="images/2.jpg" alt="">
+        <div>
+            <h6></h6>
+            <p>sgfdhjgkhgdfg</p>
+        </div>
+    </li>
+    <li>
+        <img src="images/3.jpg" alt="">
+        <div>
+            <h6></h6>
+            <p>sgfdhjgkhgdfg</p>
+        </div>
+    </li>
+    <li>
+        <img src="images/4.jpg" alt="">
+        <div>
+            <h6></h6>
+            <p>sgfdhjgkhgdfg</p>
+        </div>
+    </li>
+    <li>
+        <img src="images/5.jpg" alt="">
+        <div>
+            <h6></h6>
+            <p>sgfdhjgkhgdfg</p>
+        </div>
+    </li>
+</div>
+</body>
+</html>
